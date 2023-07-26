84544d6e48b23881196775a7f1fd1fe7ed15b649
pecheylauren02 <pechey.lauren02@gmail.com>
2023-07-26 19:13:17 +0000

    Add deploy url to allowed hosts

--- whispersoffootprints/settings.py
+++ whispersoffootprints/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-pecheylaure-mp4travelbl-dq0x3q646cj.ws-eu102.gitpod.io', 'whispers-of-footprints.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['whispers-of-footprints.herokuapp.com', 'localhost', 'whispers-of-footprints-7b4c45535e12.herokuapp.com',]
 
 
 # Application definition
