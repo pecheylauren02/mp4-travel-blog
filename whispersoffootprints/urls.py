c9d8b879f25617ff09dda1f82669ae6d6303b290
pecheylauren02 <pechey.lauren02@gmail.com>
2023-07-28 09:34:47 +0000

    Add more urls for signup

--- whispersoffootprints/urls.py
+++ whispersoffootprints/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('allauth.urls')),
+    # path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
