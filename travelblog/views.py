c7116d87c1113aa8a837eae9f1f2b817d34f5de6
pecheylauren02 <pechey.lauren02@gmail.com>
2023-08-10 17:55:18 +0000

    Add post detail view

--- travelblog/views.py
+++ travelblog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Class based view are useful for making views that are reusable, 
@@ -16,3 +16,28 @@ class PostList(generic.ListView):
     # Contents of our post table
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        # Gets the post
+        queryset = Post.objects.filter(status=1)
+        # Gets the published post with the correct slug
+        post = get_object_or_404(queryset, slug=slug)
+        # Gets comments from post and filters only those that are approved
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+      
+        # Send all of this to our render method
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
