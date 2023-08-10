3638292ad83cdb602e562c85d9ff2c4719f75876
pecheylauren02 <pechey.lauren02@gmail.com>
2023-08-10 18:09:24 +0000

    Add for loops to post detail page

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -5,4 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     # Because we are using class based views, we need to use as_view()
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    # First slug above is a path converter,
+    # which converts the slug into a text field
+    # The second slug is a keyword name
 ]
