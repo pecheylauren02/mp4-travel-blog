e5d23cc0b9257b2e130712310d7fb8179286ef1e
pecheylauren02 <pechey.lauren02@gmail.com>
2023-08-10 17:05:57 +0000

    Create view for posts

--- travelblog/views.py
+++ travelblog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
+# Class based view are useful for making views that are reusable, 
+# which can inherit from eachother
+# Create the view code
+# Create a template to render the view
+# Connect up our URLS in the url.py file
 
-# def home_view(request):
-#     return HttpResponse("Welcome to Whispers of Footprints!")
-# Create your views here.
+class PostList(generic.ListView):
+    # Based on generic listview model
+    model = Post
+    # Inside the class we are telling it to use Post as its model
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # Contents of our post table
+    template_name = 'index.html'
+    paginate_by = 6
