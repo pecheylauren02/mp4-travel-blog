881e85df771597dac784f47dcaf468fe5d707c5e
pecheylauren02 <pechey.lauren02@gmail.com>
2023-09-22 08:47:08 +0000

    Add post likes to blog posts

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    # Because we are using class based views, we need to use as_view()
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    # First slug above is a path converter,
-    # which converts the slug into a text field
-    # The second slug is a keyword name
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
