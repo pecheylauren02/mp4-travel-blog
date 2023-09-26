8df0069903a1303c70512e8d46a65fc223c7f080
pecheylauren02 <pechey.lauren02@gmail.com>
2023-09-26 08:02:13 +0000

    Add comments to models and views

--- travelblog/views.py
+++ travelblog/views.py
@@ -67,12 +67,19 @@ class PostDetail(View):
 
 
 class PostLike(View):
-  
+ 
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, slug=slug)
+        post = get_object_or_404(Post, slug=slug) # instead of slug, but comment ID, also pass user = request.user, if comment is not of the user they will be directed to user page
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+# A new view to update and delete comments
+# Change model, create a view for update
+# When the user clicks update, take the user to a new page which will have a form that is already filled in
+# The get will get the comments form with the body prefilled, and once the user clicks on save, there would need a post function which posts the forms
+# Only by registered and admin user, use login required mixin for classes, and decorator for functions
+# If a user is editting the view, it should be the comment that the user comments
--- whispersoffootprints/settings.py
+++ whispersoffootprints/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['whispers-of-footprints.herokuapp.com', 'localhost',
                  'whispers-of-footprints-7b4c45535e12.herokuapp.com',
-                 '8000-pecheylaure-mp4travelbl-dq0x3q646cj.ws-us105.gitpod.io']
+                 '8000-pecheylaure-mp4travelbl-dq0x3q646cj.ws-us104.gitpod.io']
 
 
 # Application definition
